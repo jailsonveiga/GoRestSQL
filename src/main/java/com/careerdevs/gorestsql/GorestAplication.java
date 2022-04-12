b7a97cb239561502fa8c1cc190571e810d240dfe
jailsonveiga <jmdaveiga2012@gmail.com>
2022-04-12 18:01:28 -0400

    GoRestSQL GorestApplication.java

---
+++ src/main/java/com/careerdevs/gorestsql/GorestAplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gorestsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorestAplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorestAplication.class, args);
+	}
+
+}
